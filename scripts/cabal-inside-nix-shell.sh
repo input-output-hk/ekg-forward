4ec4cc79aa19903539808bf4ab8c734f211a3746
Denis Shevchenko <denis.shevchenko@iohk.io>
2021-03-02 16:34:20 +0400

    Cabal.project init

--- scripts/cabal-inside-nix-shell.sh
+++ scripts/cabal-inside-nix-shell.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-cabal_project="$(git rev-parse --show-toplevel)"/cabal.project
-
-set -x
-sed -i 's_^    -- ../_    ../_' "$cabal_project"
-sed -ni '1,/--- 8< ---/ p'      "$cabal_project"
+sed -ni '1,/--- 8< ---/ p' "$(git rev-parse --show-toplevel)"/cabal.project
